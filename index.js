2e5beb91ff0b703f2da33741ff6b97494b17bd0b
Tarun Tomar <taruntomar022@icloud.com>
2020-07-11 22:56:39 +0530

    js file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(
         $("#grid .cell").on("click", function () {
             let rid = Number($(this).attr("ri"));
             let cid = Number($(this).attr("ci"));
-            let ciAdrr = String.fromCharCode(cid + 65);
+            let ciAdrr = String.fromCharCode(cid + 65); 
             $("#address-container").val(ciAdrr + (rid + 1));
         })
         $(".menu-items").on("click", function () {
